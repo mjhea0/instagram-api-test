fac50a6347f772a6969481b662e9e3e07483a89e
Michael Herman <hermanmu@gmail.com>
2016-06-14 16:07:50 -0600

    updated

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var accessToken = require('./_config.js').accessToken;
 
 var instagramAPI = new InstagramAPI(accessToken);
 
-instagramAPI.userSelf()
+// instagramAPI.userSelf()
+//   .then(function(result) {
+//     var userID = result.data.id;
+//     instagramAPI.userMedia(userID)
+//     .then(function(result) {
+//       console.log(result);
+//     });
+//   });
+
+instagramAPI.mediaSearch(48.4335645654, 2.345645645)
   .then(function(result) {
-    var userID = result.data.id;
-    instagramAPI.userMedia(userID)
-    .then(function(result) {
-      console.log(result);
-    });
+    console.log(result);
+  })
+  .catch(function(err) {
+    console.log(err);
   });
